8768b68659ff3af51446af2acce5943a3fbe32f9
zGadli <kartikgadli303@gmail.com>
2024-03-22 20:57:38 +0530

    Renaming files

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000; // You can change this port if needed
 
@@ -7,7 +8,24 @@ app.get('/', (req, res) => {
   res.send("Hello World Template!");
 });
 
+
+app.get('/audio/:fileName', (req, res) => {
+  // Sending the audio file when requested
+  const {fileName} = req.params
+  res.sendFile(path.join(__dirname, `/Modules/Listening/mp3 files/${ fileName }.mp3`));
+  console.log(`requested resource ${fileName}`)
+});
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
