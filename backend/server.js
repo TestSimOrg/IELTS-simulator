1dc1d050648eb7b7124a722c8d1b78422d5bf118
zGadli <kartikgadli303@gmail.com>
2024-03-23 16:38:51 +0530

    Added Listening Answer checker and answer sheet test

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import fs from 'fs/promises';
+
 const app = express();
 const port = 3000; // You can change this port if needed
 
@@ -8,24 +10,33 @@ app.get('/', (req, res) => {
   res.send("Hello World Template!");
 });
 
-
 app.get('/audio/:fileName', (req, res) => {
   // Sending the audio file when requested
-  const {fileName} = req.params
-  res.sendFile(path.join(__dirname, `/Modules/Listening/mp3 files/${ fileName }.mp3`));
-  console.log(`requested resource ${fileName}`)
+  const { fileName } = req.params;
+  res.sendFile(path.join(__dirname, `/Modules/Listening/mp3 files/${fileName}.mp3`));
+  console.log(`requested resource ${fileName}`);
 });
 
+// app.get('/l/ans/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const filePath = path.resolve(`./Modules/Listening/ans/${id}.json`);
+    
+//     // Read the JSON file asynchronously
+//     const jsonData = await fs.readFile(filePath, 'utf-8');
+    
+//     // Parse JSON data
+//     const parsedData = JSON.parse(jsonData);
+    
+//     // Send the parsed JSON as response
+//     res.json(parsedData);
+//   } catch (err) {
+//     // If an error occurs, send an error response
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
-
-
-
-
-
-
-
-
