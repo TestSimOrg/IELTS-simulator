1307c817b33bd18bbc88285771866383694355a6
zGadli <kartikgadli303@gmail.com>
2024-03-25 03:52:24 +0530

    Update summaryCompletion.js

--- backend/models/summaryCompletion.js
+++ backend/models/summaryCompletion.js
@@ -44,38 +44,6 @@ const summaryCompletionSchema = new mongoose.Schema({
 
 });
 
-/* 
-{
-    noOfWords : 1, 
-    noOfNum : 0, 
-    questionTitle : Sydney Opera House,
-    summary: "Sydney Opera House is famous for its roof's architecture resembling (32) _blank_ .The opera house was designed by Jorn Utzon from Denmark and it was built between 1959 and 33 _blank_. \n\n The roof is covered with more than 1 million roof tiles which were manufactured in (34) _blank_ .Everyevening the roof is litupina (35) _blank_ spectacle. \n\nThe opera house has a variety of performance halls and theatre and exhibition spaces.
-    More than (36) _blank_ shows are staged there every week. Every year, more than 8 million visitors visit this Australian landmark.",
-    startQuestionNum : 32,
-    endQuestionNum : 36
-} 
-
-Questions 32-36
-
-Complete the summary below.
-
-Write ONLY ONE WORD for each answer.
-
-Sydney Opera House
-
-Sydney Opera House is famous for its roof's architecture resembling 32 ................ .
-The opera house was designed by Jorn Utzon from Denmark and it was built between
-1959and33................ ...
-
-The roof is covered with more than 1 million roof tiles which were manufactured in
-
-34 ................ . Every evening the roof is litupina 35 ................ spectacle.
-
-The opera house has a variety of performance halls and theatre and exhibition spaces.
-More than 36 ................ shows are staged there every week. Every year, more than 8
-million visitors visit this Australian landmark.
-
-*/
 
 const summaryCompletionQuestion = mongoose.model('summaryCompletionQuestion', summaryCompletionSchema);
 
