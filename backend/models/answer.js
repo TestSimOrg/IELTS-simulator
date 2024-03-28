050b1311974525741cf0fef6aa09cadb876243ee
zGadli <kartikgadli303@gmail.com>
2024-03-28 14:44:36 +0530

    Add answer number to answers

--- backend/models/answer.js
+++ backend/models/answer.js
@@ -3,27 +3,31 @@ import log from '../lib/logger.js';
 
 const answerSchema = new mongoose.Schema({
     
-    ansType : {
-      type: String,
-      enum: ['S', 'M','L','IEO','B1','B2'],
-      required: true
-    },
-    qPair : {
-      type : [String],
-      required: function validate(){
-          return this.ansType === 'IEO';
-      }
-    },
-    ans: {
-      type: Schema.Types.Mixed,
-      required: true,
+  number:{
+    type: Number,
+    required: true,
+  },
+  ansType : {
+    type: String,
+    enum: ['S', 'M','L','IEO','B1','B2'],
+    required: true
+  },
+  qPair : {
+    type : [String],
+    required: function(){
+      return this.ansType === 'IEO';
     }
+  },
+  ans: {
+    type: Schema.Types.Mixed,
+    required: true,
+  }
 })
 
 answerSchema.pre('validate', function(next) {
   const ansType = this.ansType;
   const ans = this.ans;
-  log.info(`ans : ${ans}`)
+
   // Validate ans based on ansType
   if (ansType === 'S' || ansType === 'L' || ansType === 'B1') {
     if (typeof ans !== 'string') {
