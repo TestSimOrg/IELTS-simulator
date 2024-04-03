8cbfa771ec32c317768c696019f670cefabee16d
zGadli <kartikgadli303@gmail.com>
2024-04-03 21:14:55 +0530

    user controller and jwt signing setup

---
+++ backend/controllers/user.js
@@ -0,0 +1,73 @@
+import log from '../lib/logger.js';
+import user from '../models/user.js';
+import jwt from 'jsonwebtoken';
+
+const createUser = async (req, res) => {
+
+    log.info('Creating user: ', req.body);
+
+    const { username, password, email } = req.body;
+
+    try {
+
+        const userDoc = new user({
+            userName: username,
+            email: email,
+            password: password,
+        })
+        
+        const token = createToken(userDoc._id);
+
+        const resJson = (await userDoc.save()).toJSON()
+
+        log.info('Created user:', resJson);
+    
+        // Sending a response back to the client
+        // res.cookie('jwt', token/*, { httpOnly: true, maxAge: maxAge * 1000 }*/);
+        // res.cookie('jwt', token, { domain: 'localhost', path: '/', sameSite: 'None', secure: false });
+
+        // res.status(200).json({ 
+        //     message: 'User registered successfully',
+        //     obj: resJson, 
+        // });
+        res.cookie('newUser', false, { 
+            path: '/',
+            httpOnly: false, // Allow access from frontend JavaScript
+            sameSite: 'none', // For cross-site cookies
+            secure: false // If your frontend is served over HTTPS
+          });
+        res.json({
+            ok: true
+        });
+          
+        
+    } catch (err) {
+        
+        log.error('Error while registering user.', err);
+
+        res.status(500).json({
+            message: "Internal server error",
+            ok: false,
+            status: 500
+        })
+    }
+}
+
+const maxAge = 3 * 24 * 60 * 60; // 3 days
+const createToken = (id) => {
+    const token = jwt.sign({ id },'256-secret', {
+        expiresIn: maxAge
+    });
+
+    log.info(token)
+
+    return token;
+}
+
+const userQuestions = async (req, res) => {
+
+}
+
+const userController = { createUser, userQuestions };
+
+export default userController;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,27 +1,8 @@
 import express from 'express';
-import user from '../models/user.js';
+import userController from '../controllers/user.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/', async (req, res) => {
-
-    console.log(req.body)
-    const { username, password, email } = req.body;
-
-    const userDoc = new user({
-        userName: username,
-        email: email,
-        password: password,
-    })
-
-    const resJson = (await userDoc.save()).toJSON()
-    console.log('Stored data:', resJson);
-
-    // Sending a response back to the client
-    return res.status(200).json({ 
-        message: 'User registered successfully',
-        obj: resJson, 
-    });
-});
+userRouter.post('/', userController.createUser);
 
 export default userRouter;
