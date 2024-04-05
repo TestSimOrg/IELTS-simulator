c75108afbe3216a8d1d13df5163eb085eb260fbd
zGadli <kartikgadli303@gmail.com>
2024-04-05 14:30:20 +0530

    add user/login route to user router

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,8 @@ import userController from '../controllers/user.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/', userController.createUser);
+userRouter.post('/signin', userController.createUser); // Register
+
+userRouter.post('/login', userController.userLogin); //Login
 
 export default userRouter;
