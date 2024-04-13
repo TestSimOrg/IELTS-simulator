5cd1151ca7ee279247d108a5d27a231ea04941ae
zGadli <kartikgadli303@gmail.com>
2024-04-13 21:38:51 +0530

    Align question headers and more to left

--- frontend/src/components/questions/commons/Question.jsx
+++ frontend/src/components/questions/commons/Question.jsx
@@ -5,7 +5,7 @@ import { Text } from "@mantine/core";
 export const Question = ({ questionTitle, questionHeader, questionStatement, children }) => {
 	return (
 		<div style={{ marginBottom: "100px" }}>
-			<Text size="lg">{questionTitle}</Text>
+			<Text ta={"left"} size="lg">{questionTitle}</Text>
 			<Text size="md">{questionHeader}</Text>
 			<Text size="sm">{questionStatement}</Text>
 			<div style={{ marginTop: "20px" }}>
--- frontend/src/components/questions/commons/QuestionImage.jsx
+++ frontend/src/components/questions/commons/QuestionImage.jsx
@@ -8,10 +8,12 @@ import React from "react";
 
 export const QuestionImage = ({ image }) => {
     return (
+        <div style={{ display: "flex", justifyContent: "center" }}>
         <img
             src={`data:${image.contentType};base64,${image.data}`}
             alt="question"
             style={{ width: "50%" }}
         />
+        </div>
     );
 }
