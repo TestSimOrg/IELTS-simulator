9d80f044c2a2f26f4a2b5ddc1894f8a84199c19f
zGadli <kartikgadli303@gmail.com>
2024-04-15 21:02:53 +0530

    export question json so you can import it anywhere

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -2,7 +2,7 @@
 //
 // Path: frontend/src/pages/Test.jsx
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Container, Typography } from "@mui/material";
 import { Matching } from "../components/questions/Matching";
 import { SentenceCompletion } from "../components/questions/SentenceCompletion";
@@ -15,230 +15,232 @@ import { YNNG } from "../components/questions/YNNG";
 import { TFNG } from "../components/questions/TFNG";
 import imgBase64 from "../assets/imgExample";
 
-export default function Test() {
-    var qMatching = {
-        startQuestionNum: 21,
-        endQuestionNum: 25,
-        standAlone: true,
-        numOfWords: 1,
-        numOfNum: 0,
-        questionHeader:
-            "Write the correct letter, A, B or C next to question 21-25.\nYou may choose any letter more than once.",
-        questionOptionRepeatable: true,
-        questionStatment:
-            "What does Jack tell his tutor about each of the following course option?",
-        questionOptions: [
-            "A He'll definitely do it.",
-            "B He may or may not do it.",
-            "C He won't do it.",
-        ],
-        numStatements: [
-            "21 Media Studies _BLANK_",
-            "22 Women and Power _BLANK_",
-            "23 Culture and Society _BLANK_",
-            "24 Identify and Popular Culture _BLANK_",
-            "25 Introduction to Culture Theory _BLANK_",
-        ],
-    };
+export const qMatching = {
+    startQuestionNum: 21,
+    endQuestionNum: 25,
+    standAlone: true,
+    numOfWords: 1,
+    numOfNum: 0,
+    questionHeader:
+        "Write the correct letter, A, B or C next to question 21-25.\nYou may choose any letter more than once.",
+    questionOptionRepeatable: true,
+    questionStatment:
+        "What does Jack tell his tutor about each of the following course option?",
+    questionOptions: [
+        "A He'll definitely do it.",
+        "B He may or may not do it.",
+        "C He won't do it.",
+    ],
+    numStatements: [
+        "21 Media Studies _BLANK_",
+        "22 Women and Power _BLANK_",
+        "23 Culture and Society _BLANK_",
+        "24 Identify and Popular Culture _BLANK_",
+        "25 Introduction to Culture Theory _BLANK_",
+    ],
+};
 
-    const qYNNG = {
-        startQuestionNum: 7,
-        endQuestionNum: 12,
-        standAlone: true,
-        questionHeader: "Do the following statements agree with the views of the writer in the Reading Passage?\nWrite:\n\tYES if the statement agrees with the views of the writer.\n\tNO if the statement contradicts what the writer thinks.\n\tNOT GIVEN if it is impossible to know what the writer's point of view is.",
-        numStatements: [
-            "7\tChildren can learn their first language without being taught.",
-            "8\tFrom the time of their birth, humans seem to have an ability to learn language",
-            "9\tAccording to experts in the 1950s and '60s, language learning is very similar to the training of animals.",
-            "10\tRepetition in language learning is important, according to Dr Eliot.",
-            "11\tDr Golinkoff is concerned that \"baby talk\" is spoken too much by some parents.",
-            "12\tThe first word a child learns to recognise is usually \"Mummy\" or \"Daddy\"."
-        ]
-    }
+export const qYNNG = {
+    startQuestionNum: 7,
+    endQuestionNum: 12,
+    standAlone: true,
+    questionHeader: "Do the following statements agree with the views of the writer in the Reading Passage?\nWrite:\n\tYES if the statement agrees with the views of the writer.\n\tNO if the statement contradicts what the writer thinks.\n\tNOT GIVEN if it is impossible to know what the writer's point of view is.",
+    numStatements: [
+        "7\tChildren can learn their first language without being taught.",
+        "8\tFrom the time of their birth, humans seem to have an ability to learn language",
+        "9\tAccording to experts in the 1950s and '60s, language learning is very similar to the training of animals.",
+        "10\tRepetition in language learning is important, according to Dr Eliot.",
+        "11\tDr Golinkoff is concerned that \"baby talk\" is spoken too much by some parents.",
+        "12\tThe first word a child learns to recognise is usually \"Mummy\" or \"Daddy\"."
+    ]
+}
 
-    const qTFNG = {
-        startQuestionNum: 9,
-        endQuestionNum: 13,
-        standAlone: true,
-        questionHeader: "Do the following statements agree with the views of the writer in the Reading Passage?\nIn boxes 9-13 on your answer write:\n\tTRUE if the statement agrees with the views of the information.\n\tNO if the statement contradicts the information.\n\tNOT GIVEN if there is no information on this.",
-        numStatements: [
-            "9\tCoconut seeds need shade in order to germinate.",
-            "11\tCoconuts were probably transported to Asia from America in the 16th century.",
-            " 12\tCoconuts found on the west coast of America were a different type from those found on the east coast.",
-            "13\tAll coconuts found in Asia are cultivated varieties.",
-            "14\tCoconuts are cultivated in different ways in America and the Pacific."
-        ]
-    }
+export const qTFNG = {
+    startQuestionNum: 9,
+    endQuestionNum: 13,
+    standAlone: true,
+    questionHeader: "Do the following statements agree with the views of the writer in the Reading Passage?\nIn boxes 9-13 on your answer write:\n\tTRUE if the statement agrees with the views of the information.\n\tNO if the statement contradicts the information.\n\tNOT GIVEN if there is no information on this.",
+    numStatements: [
+        "9\tCoconut seeds need shade in order to germinate.",
+        "11\tCoconuts were probably transported to Asia from America in the 16th century.",
+        " 12\tCoconuts found on the west coast of America were a different type from those found on the east coast.",
+        "13\tAll coconuts found in Asia are cultivated varieties.",
+        "14\tCoconuts are cultivated in different ways in America and the Pacific."
+    ]
+}
 
-    var qSentenceCompletion = {
-        startQuestionNum: 27,
-        endQuestionNum: 30,
-        standAlone: true,
-        numOfWords: 2,
-        numOfNum: 0,
-        questionHeader:
-            "Complete the sentences below.\nWrite NO MORE THAN TWO WORDS ONLY for each answer.",
-        questionTitle: "",
-        numStatements: [
-            "27 Studying with the Open University demanded a great deal of _BLANK_ ",
-            "28 Studying and working at the same time improved Rachel's _BLANK_ skills.",
-            "29 It was helpful that the course was structured in _BLANK_.",
-            "30 She enjoyed meeting other students at _BLANK_."
-        ],
-    };
+export const qSentenceCompletion = {
+    startQuestionNum: 27,
+    endQuestionNum: 30,
+    standAlone: true,
+    numOfWords: 2,
+    numOfNum: 0,
+    questionHeader:
+        "Complete the sentences below.\nWrite NO MORE THAN TWO WORDS ONLY for each answer.",
+    questionTitle: "",
+    numStatements: [
+        "27 Studying with the Open University demanded a great deal of _BLANK_ ",
+        "28 Studying and working at the same time improved Rachel's _BLANK_ skills.",
+        "29 It was helpful that the course was structured in _BLANK_.",
+        "30 She enjoyed meeting other students at _BLANK_."
+    ],
+};
 
-    var qMultipleChoiceType1 = {
-        startQuestionNum: 15,
-        endQuestionNum: 17,
-        standAlone: true,
-        numOfWords: 1,
-        qType: 1,
-        questionHeader: ["Choose the correct letter, A, B or C."],
-        numStatements: [
-            "15 How does BC Travel plan to expand the painting holidays?",
-            "16 Why are BC Travel's cooking holidays unusual?",
-            "17 What does the speaker say about the photography holidays?",
-        ],
-        questionStatements: [
-            [
-                "A by adding to the number of locations",
-                "B by increasing the range of levels",
-                "C by employing more teachers",
-            ],
-            [
-                "A They only use organic foods",
-                "B They have an international focus",
-                "C They mainly involve vegetarian dishes",
-            ],
-            [
-                "A Clients receive individual tution.",
-                "B The tutors are also trained guides.",
-                "C Advice is given on selling photographs.",
-            ],
-        ],
-    };
-
-    var qMultipleChoiceType2 = {
-        startQuestionNum: 11,
-        endQuestionNum: 14,
-        standAlone: true,
-        numOfWords: 1,
-        numOfNum: 0,
-        qType: 2,
-        questionHeader: ["Choose TWO letter, A-E", "Choose THREE letter, A-E"],
-        numStatements: [
-            "Which TWO age groups are taking increasing numbers of holiday with BC Travel?",
-            "Which TWO are the main reasons given for the popularity of activity holidays?",
-        ],
-        questionOptions: [
-            [
-                "A 16-30 years",
-                "B 31-42 years",
-                "C 43-54 years",
-                "D 55-64 years",
-                "E over 65 years",
-            ],
-            [
-                "A Clients make new friends.",
-                "B Clients learn a useful skill.",
-                "C Clients learn about a different culture.",
-                "D Clients are excited by the risk involved.",
-                "E Clients find them value for money.",
-            ],
-        ],
-    };
-
-    const qDiagramCompletion = {
-        startQuestionNum: 14,
-        endQuestionNum: 20,
-        standAlone: true,
-        options: true,
-        questionHeader:
-            "Label the map below.\nWrite the correct letter, A-I, next to Questions 14-20",
-        questionTitle: "Proposed traffic changes in Granford",
-        image: {
-            data: imgBase64,
-            contentType: "image/png",
-        },
-        questionOptions: ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
-        numStatements: [
-            "14 New traffic lights _BLANK_",
-            "15 Pedestrian crossing _BLANK_",
-            "16 Parking allowed _BLANK_",
-            "17 New 'No Parking' sign _BLANK_",
-            "18 New disabled parking spaces _BLANK_",
-            "19 Widened pavement _BLANK_",
-            "20 Lorry loading/unloading restrictions _BLANK_"
-        ],
-    };
-
-    const qNoteCompletion = {
-        startQuestionNum: 11,
-        endQuestionNum: 16,
-        standAlone: true,
-        numOfWords: 3,
-        numOfNum: 1,
-        questionHeader: 'Complete the notes below.\nWrite NO MORE THAN THREE WORDS AND/OR A NUMBER for each answer.',
-        questionTitle: 'The National Arts Centre',
-        questionStatements: 
+export const qMultipleChoiceType1 = {
+    startQuestionNum: 15,
+    endQuestionNum: 17,
+    standAlone: true,
+    numOfWords: 1,
+    qType: 1,
+    questionHeader: ["Choose the correct letter, A, B or C."],
+    numStatements: [
+        "15 How does BC Travel plan to expand the painting holidays?",
+        "16 Why are BC Travel's cooking holidays unusual?",
+        "17 What does the speaker say about the photography holidays?",
+    ],
+    questionStatements: [
         [
-            'Well known for:\t11 _BLANK_.',
-            'Complex consists of:\tconcert rooms\ntheatres\ncinemas\nart galleries\npublic library\nrestaurants\n12 _BLANK_',
-            'Historical background:\t1940 area dostoyed by bombs\n19605 — Centre was 13 _BLANK_\nIn 14 _BLANK_ opened to public',
-            'Managed by:\t15 _BLANK_',
-            'Open:\t16 _BLANK_ days per year'
+            "A by adding to the number of locations",
+            "B by increasing the range of levels",
+            "C by employing more teachers",
         ],
-    };
+        [
+            "A They only use organic foods",
+            "B They have an international focus",
+            "C They mainly involve vegetarian dishes",
+        ],
+        [
+            "A Clients receive individual tution.",
+            "B The tutors are also trained guides.",
+            "C Advice is given on selling photographs.",
+        ],
+    ],
+};
 
-    const qFlowChart = {
-        "startQuestionNum": 31,
-        "endQuestionNum": 34,
-        "standAlone": true,
-        "options": false,
-        "numOfWords": 2,
-        "numOfNum": 2,
-        "questionHeader": "You will hear an extract from a university lecture on the topic of economics.\nFirst, look at questions 31 to 34. Now listen carefully and answer questions 31-34.\nQuestions 31-34. Do not write MORE THAN TWO WORDS AND/OR NUMBERS. Complete the chart below.",
-        "steps": [
-            [
-                "Effects of 2020 Pandemic"
-            ],
-            [
-                "31 _BLANK_ changed"
-            ],
-            [
-                " Demand for 32 _BLANK_ in stores increased"
-            ],
-            [
-                "Prices increased by more than 2%"
-            ],
-            [
-                "33 _BLANK_ went up by 2.4%",
-                "Protein went up by 34 _BLANK_"
-            ]
+export const qMultipleChoiceType2 = {
+    startQuestionNum: 11,
+    endQuestionNum: 14,
+    standAlone: true,
+    numOfWords: 1,
+    numOfNum: 0,
+    qType: 2,
+    questionHeader: ["Choose TWO letter, A-E", "Choose THREE letter, A-E"],
+    numStatements: [
+        "Which TWO age groups are taking increasing numbers of holiday with BC Travel?",
+        "Which TWO are the main reasons given for the popularity of activity holidays?",
+    ],
+    questionOptions: [
+        [
+            "A 16-30 years",
+            "B 31-42 years",
+            "C 43-54 years",
+            "D 55-64 years",
+            "E over 65 years",
         ],
-        "answer": [
-            {
-                "number": 31,
-                "ansType": "S",
-                "ans": "consumer habits"
-            },
-            {
-                "number": 32,
-                "ansType": "M",
-                "ans": ["products", "food"]
-            },
-            {
-                "number": 33,
-                "ansType": "S",
-                "ans": "cereals"
-            },
-            {
-                "number": 34,
-                "ansType": "S",
-                "ans": "4.3%"
-            }
+        [
+            "A Clients make new friends.",
+            "B Clients learn a useful skill.",
+            "C Clients learn about a different culture.",
+            "D Clients are excited by the risk involved.",
+            "E Clients find them value for money.",
+        ],
+    ],
+};
+
+export const qDiagramCompletion = {
+    startQuestionNum: 14,
+    endQuestionNum: 20,
+    standAlone: true,
+    options: true,
+    questionHeader:
+        "Label the map below.\nWrite the correct letter, A-I, next to Questions 14-20",
+    questionTitle: "Proposed traffic changes in Granford",
+    image: {
+        data: imgBase64,
+        contentType: "image/png",
+    },
+    questionOptions: ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
+    numStatements: [
+        "14 New traffic lights _BLANK_",
+        "15 Pedestrian crossing _BLANK_",
+        "16 Parking allowed _BLANK_",
+        "17 New 'No Parking' sign _BLANK_",
+        "18 New disabled parking spaces _BLANK_",
+        "19 Widened pavement _BLANK_",
+        "20 Lorry loading/unloading restrictions _BLANK_"
+    ],
+};
+
+export const qNoteCompletion = {
+    startQuestionNum: 11,
+    endQuestionNum: 16,
+    standAlone: true,
+    numOfWords: 3,
+    numOfNum: 1,
+    questionHeader: 'Complete the notes below.\nWrite NO MORE THAN THREE WORDS AND/OR A NUMBER for each answer.',
+    questionTitle: 'The National Arts Centre',
+    questionStatements: 
+    [
+        'Well known for:\t11 _BLANK_.',
+        'Complex consists of:\tconcert rooms\ntheatres\ncinemas\nart galleries\npublic library\nrestaurants\n12 _BLANK_',
+        'Historical background:\t1940 area dostoyed by bombs\n19605 — Centre was 13 _BLANK_\nIn 14 _BLANK_ opened to public',
+        'Managed by:\t15 _BLANK_',
+        'Open:\t16 _BLANK_ days per year'
+    ],
+};
+
+export const qFlowChart = {
+    "startQuestionNum": 31,
+    "endQuestionNum": 34,
+    "standAlone": true,
+    "options": false,
+    "numOfWords": 2,
+    "numOfNum": 2,
+    "questionHeader": "You will hear an extract from a university lecture on the topic of economics.\nFirst, look at questions 31 to 34. Now listen carefully and answer questions 31-34.\nQuestions 31-34. Do not write MORE THAN TWO WORDS AND/OR NUMBERS. Complete the chart below.",
+    "steps": [
+        [
+            "Effects of 2020 Pandemic"
+        ],
+        [
+            "31 _BLANK_ changed"
+        ],
+        [
+            " Demand for 32 _BLANK_ in stores increased"
+        ],
+        [
+            "Prices increased by more than 2%"
+        ],
+        [
+            "33 _BLANK_ went up by 2.4%",
+            "Protein went up by 34 _BLANK_"
         ]
-    };
+    ],
+    "answer": [
+        {
+            "number": 31,
+            "ansType": "S",
+            "ans": "consumer habits"
+        },
+        {
+            "number": 32,
+            "ansType": "M",
+            "ans": ["products", "food"]
+        },
+        {
+            "number": 33,
+            "ansType": "S",
+            "ans": "cereals"
+        },
+        {
+            "number": 34,
+            "ansType": "S",
+            "ans": "4.3%"
+        }
+    ]
+};
+
+export default function Test() {
+
 
     return (
         <div>
