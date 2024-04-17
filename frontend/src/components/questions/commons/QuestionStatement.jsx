16c772b09a819f50cc1882607773f81d407c8c7a
zGadli <kartikgadli303@gmail.com>
2024-04-17 23:26:27 +0530

    Add question statement abstraction

---
+++ frontend/src/components/questions/commons/QuestionStatement.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Text } from '@mantine/core'
+export const QuestionStatement = ({ qStatement }) => {
+  return (
+    <Text size="md" pl={5}>{qStatement}</Text>
+  )
+}
