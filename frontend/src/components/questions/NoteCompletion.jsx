c630617b734aecaaae31fb1d2c11743ee086e157
zGadli <kartikgadli303@gmail.com>
2024-04-19 20:50:16 +0530

    Update NoteCompletion.jsx

--- frontend/src/components/questions/NoteCompletion.jsx
+++ frontend/src/components/questions/NoteCompletion.jsx
@@ -1,28 +1,7 @@
-// const qNoteCompletion = {
-//     startQuestionNum: 11,
-//     endQuestionNum: 16,
-//     standAlone: true,
-//     numOfWords: 3,
-//     numOfNum: 1,
-//     questionHeader:
-//         "Complete the notes below.\nWrite NO MORE THAN THREE WORDS AND/OR A NUMBER for each answer.",
-//     questionTitle: "The National Arts Centre",
-//     questionStatements: [
-//         "Well known for:\t11 _BLANK_.",
-//         "Complex consists of:\tconcert rooms\ntheatres\ncinemas\nart galleries\npublic library\nrestaurants\n12 _BLANK_",
-//         "Historical background:\t1940 area dostoyed by bombs\n19605 — Centre was 13 _BLANK_\nIn 14 _BLANK_ opened to public",
-//         "Managed by:\t15 _BLANK_",
-//         "Open:\t16 _BLANK_ days per year",
-//     ],
-// };
-
-// question/NoteCompletion.jsx
 import React from "react";
 import { Text, Grid, Container } from "@mantine/core";
-import { Question } from "./commons/Question";
 import { QuestionInput } from "./commons/QuestionInput";
 import { QuestionHeader } from "./commons/QuestionHeader";
-import { QuestionStatement } from "./commons/QuestionStatement";
 import { QuestionTitle } from "./commons/QuestionTitle";
 
 
