47c9c474a0f3dae39cd1f0c81b35bb5129d83f18
zGadli <kartikgadli303@gmail.com>
2024-04-20 18:54:07 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <MantineProvider>
             <HeaderMegaMenu />
 		        <App />
-            < Footer />
+            <Footer />
         </MantineProvider>
 	</Router>
 );
