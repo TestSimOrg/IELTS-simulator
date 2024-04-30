2730d830e2ad7ae35c323080519f3fcb8d1b1bdb
zGadli <kartikgadli303@gmail.com>
2024-04-30 12:37:13 +0530

    fix route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ import userController from '../controllers/user.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/signin', userController.createUser); // Register
+userRouter.post('/signup', userController.createUser); // Register
 
 userRouter.post('/login', userController.userLogin); //Login
 
