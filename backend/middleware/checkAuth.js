bb347677dacbda73a5ca5ede33b75b79e2fd745d
zGadli <kartikgadli303@gmail.com>
2024-05-01 18:07:40 +0530

    formatting and logging

--- backend/middleware/checkAuth.js
+++ backend/middleware/checkAuth.js
@@ -1,56 +1,48 @@
-import jwt from 'jsonwebtoken';
-import log from '../lib/logger.js';
-import user from '../models/user.js'
+import jwt from "jsonwebtoken";
+import log from "../lib/logger.js";
+import user from "../models/user.js";
 
-const checkAuth =  (req, res, next) => {
+const checkAuth = (req, res, next) => {
+	const token = req.cookies.jwt;
 
-  const token = req.cookies.jwt;
+	try {
+		if (token) {
+			const decodedToken = jwt.verify(token, process.env.SECRET);
+			log.info("Decoded JWT:", decodedToken);
+			next();
+		} else {
 
-  try {
-
-    if (token) {
-
-      const decodedToken = jwt.verify(token, process.env.SECRET);
-      log.info('Decoded JWT:',decodedToken);
-      next();
-
-    } else {
-
-      
-      res.redirect('/user/login');
-
-    }
-
-  } catch (err) {
-
-    log.error('Auth error:', err);
-    res.redirect('/user/login');
-
-  }
-
+			log.info("JWT Not found on a protected route");
+			res.redirect("/user/login");
+		}
+	} catch (err) {
+		log.error("Auth error:", err);
+		res.redirect("/user/login");
+	}
 };
 
 const checkUser = (req, res, next) => {
-  
-  const token = req.cookies.jwt;
+	const token = req.cookies.jwt;
 
-  if (token) {
-    jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
-      if (err) {
-        res.locals.user = null;
-        next();
-      } else {
-        let userData = await user.findById(decodedToken.id);
-        log.info('User automatically logged in with email:', userData.email);
-        res.locals.user = userData;
-        next();
-      }
-    });
-  } else {
-    res.locals.user = null;
-    next();
-  }
-
-}
-const auth = {checkAuth, checkUser}; 
-export {checkAuth, checkUser};
+	if (token) {
+		jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
+			if (err) {
+				res.locals.user = null;
+				next();
+			} else {
+				let userData = await user.findById(decodedToken.id);
+				log.info(
+					"User automatically logged in with email:",
+					userData.email
+				);
+				res.locals.user = userData;
+				next();
+			}
+		});
+	} else {
+		res.locals.user = null;
+		next();
+	}
+};
+const auth = { checkAuth, checkUser };
+export { checkAuth, checkUser };
